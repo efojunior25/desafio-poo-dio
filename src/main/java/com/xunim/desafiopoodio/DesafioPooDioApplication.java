33b27f8598d0db90d4f19e7cbb46dcc9d6e38508
Edson Júnior <efojunior25@gmail.com>
2025-08-15 14:49:33 -0300

    First Commit

---
+++ src/main/java/com/xunim/desafiopoodio/DesafioPooDioApplication.java
@@ -0,0 +1,13 @@
+package com.xunim.desafiopoodio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPooDioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioPooDioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xunim/desafiopoodio/DesafioPooDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xunim.desafiopoodio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPooDioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
