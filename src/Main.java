949d1c354c8f36b397216c534951b5058432f01c
Edson Júnior <efojunior25@gmail.com>
2025-09-10 16:15:21 -0300

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 package src;
 
+import src.br.com.dio.desafio.dominio.Bootcamp;
 import src.br.com.dio.desafio.dominio.Curso;
+import src.br.com.dio.desafio.dominio.Dev;
 import src.br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -23,9 +25,42 @@ public class Main {
         mentoria1.setData(LocalDate.now());
 
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria1);
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria1);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria1);
+
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.InscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos de Camila: " + devCamila.getConteudosInscritos());
+        devCamila.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos de Camila: " + devCamila.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos de Camila: " + devCamila.getConteudosConcluidos());
+        System.out.println("Xp de Camila: " + devCamila.calcularXp());
+
+        System.out.println("------");
+
+        Dev devEdson = new Dev();
+        devEdson.setNome("Edson");
+        devEdson.InscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos de Edson: " + devEdson.getConteudosInscritos());
+        devEdson.progredir();
+        devEdson.progredir();
+        devEdson.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos de Edson: " + devEdson.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos de Edson: " + devEdson.getConteudosConcluidos());
+        System.out.println("Xp de Edson: " + devEdson.calcularXp());
+
+
     }
 }
 
